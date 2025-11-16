6b6800b47fb67ea59c162e8c9fa2b2339758e4d5
Bonesteeler <theo.ebenhoech@gmail.com>
2025-11-16 10:48:37 -0800

    Some more set detail polish

--- ui/set_manager/tile_preview.gd
+++ ui/set_manager/tile_preview.gd
@@ -5,12 +5,15 @@ const DISTANCE_PER_UNIT: float = 60.0
 const ROTATION_SPEED: float = 0.66
 const VERTICAL_OFFSET: float = 25.0
 
+@export var rotating: bool = false
+
 @onready var camera: Camera3D = $Camera3D
 @onready var mesh_instance: MeshInstance3D = $%PreviewMesh
 @onready var rotator: Node3D = $Rotator
 
 func _process(delta: float) -> void:
-  rotator.rotate_y(ROTATION_SPEED * delta)
+  if rotating:
+    rotator.rotate_y(ROTATION_SPEED * delta)
 
 func set_tile(tile: Tile) -> void:
   mesh_instance.mesh = load(tile.mesh_path)
@@ -24,3 +27,6 @@ func set_tile(tile: Tile) -> void:
   var max_size = max(tile.x_size, tile.y_size)
   max_size = max(max_size, 1)
   camera.position = Vector3(0, max_size * DISTANCE_PER_UNIT + VERTICAL_OFFSET,  max_size * DISTANCE_PER_UNIT)
+
+func set_rotating(value: bool) -> void:
+  rotating = value
